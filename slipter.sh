4c2fe9255a2afed961f12a1c2b0309f53aee8349
megachonker <55107313+megachonker@users.noreply.github.com>
2021-01-02 15:49:42 +0100

    premiercomit

---
+++ slipter.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+echo "fichier cache du  blender"
+#blender-temp
+pmv(){
+    source=$1;
+    target=$2;
+    mkdir -p "$target"/"$(dirname $source)"
+    mv "$source" "$target"/"$(dirname $source)"/
+}
+export -f pmv
+
+cd $1
+rm .swp
+compte=1
+var=../partie-1
+mkdir $var
+while true ;do
+	while [ $(du  -s|grep [0-9]*  -o) -gt 500000 ]; do
+		find -type f|sort  -t 0 -k 2|tail -n  3|xargs -I {} -t bash -c 'pmv "$@"' _ {} $var
+	done
+	curant=$PWD
+	foldername=$(echo ../part_$(find -type f|head -n 1|grep [0-9]* -o)-$(find -type f|tail  -n 1 |grep  [0-9]*  -o))
+	cd $var
+	if [ $(du  -s|grep [0-9]*  -o) -lt 500000 ];then
+		break
+	fi
+	mv $curant $foldername
+
+	var=../partie-$((compte+=1))
+	mkdir $var
+done
+
+find -type d -maxdepth 1 -regex ".*part_.*"|xargs -I {} zip {}.zip {} *.blend -r
